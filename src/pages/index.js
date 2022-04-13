8909f39056d70ceb084b16b350ae95d2327e26a9
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-04-13 17:22:17 +0430

    delete HomepageExample

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import HomepageHeader from "../components/HomepageHeader";
-import HomepageExample from "../components/HomepageExample";
 import HomepageSponsors from "../components/HomepageSponsors";
 
 export default function Home() {
@@ -9,7 +8,6 @@ export default function Home() {
     <Layout>
       <HomepageHeader />
       <main>
-        <HomepageExample />
         <HomepageSponsors />
       </main>
     </Layout>
